5c2d33937cce3eca85782a9bda7b1c096334daba
Nathan J Sutjiono <nsutjiono0017@floridapoly.edu>
2025-11-12 18:45:49 +0000

    2

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-print ("Welcome to rock paper scissors")
+#git init - initalizes a new git repository
+#git status - shows status of changes
+#git add - adds files to staging area. Staging area is what we define, and its the files or folders we want to save
+#git commit - save changes (comment your changes) (should be executed after specific changes)
+#git log - shows commit history
+#git diff <file> - shows unstaged changes
+#git restore <file> - restores to the previous change
+#git branch - list all branches
+#git branch <name> - make new name
+#git checkout -b - ??
+#git merge - 
+
+
+#GIT PUSH -f
+
+print ("Welcome to rock paper scissors!")
+choice = str(input("Enter rock paper, or scissors: "))
+print("You choose ", choice)
